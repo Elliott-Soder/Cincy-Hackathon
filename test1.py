f11e63967144022cc1b3ed7cbc18a689cf3eaac9
Christ Degni <145073440+c-degni@users.noreply.github.com>
2024-02-24 15:00:16 -0500

    update 2

--- test1.py
+++ test1.py
@@ -4,3 +4,27 @@ def mathingFunction(x,y):
   z = x + y
   return z
   print('Hello world')
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>
+            headings
+        </title>
+    </head>
+    <body>
+        <h1>
+            Chapter One
+        </h1>
+        <p>
+        Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+        </p>
+        <p>
+        Lorem ipsum
+        </p>
+        <p>
+        Lorem ipsum
+        </p>
+    </body>
+</html>
